ffabd3e83a147ab66555b7bdc473c1b7c1ef6a0d
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-01 09:20:22 +0100

    feat: Create function to save smurfs to state after fetching

--- village/src/App.js
+++ village/src/App.js
@@ -22,13 +22,18 @@ class App extends Component {
   fetchData = () => {
     axios
       .get(smurfsURL)
-      .then(res => console.log(res.data))
+      .then(res => this.setSmurfs(res.data))
       .catch(res => console.log(res.message));
+  };
+
+  setSmurfs = smurfs => {
+    this.setState({ smurfs });
   };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    console.log(this.state.smurfs);
     return (
       <div className="App">
         <SmurfForm />
